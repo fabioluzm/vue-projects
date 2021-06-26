0dcae95e759e09c8b163cd9cbfda8c640032a906
fabioluzm <fabioluzm@gmail.com>
2021-06-27 00:45:03 +0100

    add todos,addtodo,crud methods

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo @add-todo="addTodo"/>
+    <Todos :todos="todos" @delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header.vue'
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    async getTodos() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+      const data = await res.json();
+      this.todos = data;
+    },
+    async addTodo(newTodo) {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({newTodo})
+      });
+      const data = await res.json();
+      this.todos = [...this.todos, data];
+    },
+    async deleteTodo(id) {
+      await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'DELETE'
+      });
+      this.todos = this.todos.filter(todo => todo.id != id)
+    }
+  },
+  created() {
+    this.getTodos();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border:none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem :todo="todo" @delete-todo="$emit('delete-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+  name: 'Todos',
+  components: {
+    TodoItem
+  },
+  props: ['todos']
+}
+</script>
